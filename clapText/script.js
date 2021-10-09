9e2ead7566c1ace1f77a3c1b6798a4ab4e1195d8
Sushmey <71403193+Sushmey@users.noreply.github.com>
2021-10-09 15:33:18 +0530

    Add files via upload

---
+++ clapText/script.js
@@ -0,0 +1,19 @@
+var emoji ="👏";
+function setEmoji(){
+	emoji = document.getElementById("emoji").value;
+}
+function clapClapClap() {
+//var emoji = "👏";		
+  var x = document.getElementById("myInput").value;
+  document.getElementById("gen").innerHTML = x.split(/\s+/).join(` ${emoji} `);
+}
+function copy(){
+	 var copyText = document.getElementById("gen");
+	 console.log("Bitch!");
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+}
